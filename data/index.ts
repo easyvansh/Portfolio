95b9e2f862329bb94ab7751ca7be31d63af4f3eb
Vansh Singh <vansh2@ualberta.ca>
2024-08-17 23:19:27 +0530

    Updated Data

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems = [
+    { title: "Title 1", description: "Description", id: "1" },
+    { title: "Title 2", description: "Description", id: "1" },
+    { title: "Title 3", description: "Description", id: "1" },
+    { title: "Title 1", description: "Description", id: "1" },
+
+  ]
