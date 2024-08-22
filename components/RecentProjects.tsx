79b4890c90a493bf03d8594736e6822e653b607c
Vansh Singh <vansh2@ualberta.ca>
2024-08-22 19:04:52 +0530

    Updated Styling Project Cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,35 +13,36 @@ const RecentProjects = () => {
           A small selection of{" "}
           <span className="text-purple">recent projects</span>
         </h1>
-        <div className="flex flex-wrap items-center justify-center p-4 gap-32 mt-10">
+        <div className="flex flex-wrap items-center justify-center p-4 gap-x-32 gap-y-20 mt-10">
           {projects.map((project, index) => (
             <div
               key={index}
               className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             >
-              <CardContainer className="inter-var">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                  >
-                    {project.title}
+              <CardContainer className="inter-var ">
+                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-inherit dark:border-white/[0.2] border-black/[0.1]  flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10" >
+                  <CardItem as="h1" translateZ="50" className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                
+                      {project.title}
+                   
                   </CardItem>
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                  >
-                    {project.des}
+                  <CardItem as="p" translateZ="60" className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                      style={{
+                        color: "#BEC1DD",
+                        margin: "1vh 0",
+                      }}>
+                   
+                      {project.des}
+                    
                   </CardItem>
-                  <CardItem translateZ="100" className="w-full my-4">
+                  <CardItem translateZ="80" className="w-full my-4">
                     <Image
                       src={project.img}
                       height={1000}
                       width={1000}
                       className="h-60 w-full object-cover rounded-3xl group-hover/card:shadow-xl"
-                      alt={project.title} // Use project title or relevant description
-                      priority={index < 3} // Optional: Load the first few images with higher priority
+                      alt={project.title}
+                      priority={index < 3}
                     />
                   </CardItem>
                   <div className="flex items-center justify-between mt-7 mb-3">
@@ -61,7 +62,11 @@ const RecentProjects = () => {
                               transform: `translateX(-${5 * iconIndex + 2}px)`,
                             }}
                           >
-                            <img src={icon} alt={`icon-${iconIndex}`} className="p-2" />
+                            <img
+                              src={icon}
+                              alt={`icon-${iconIndex}`}
+                              className="p-2"
+                            />
                           </div>
                         ))}
                       </div>
